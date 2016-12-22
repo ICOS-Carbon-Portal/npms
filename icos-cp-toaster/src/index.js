626269fa012dd0fb0fc53bf9658cfd2fa9d6b83b
Roger Groth <roger.groth@gis.lu.se>
2016-12-22 11:07:16 +0100

    Created npm packages for toaster and fade

---
+++ icos-cp-toaster/src/index.js
@@ -0,0 +1,11 @@
+import * as TD from './ToasterData';
+import * as T from './Toaster';
+
+export const ToasterData = TD.ToasterData;
+export const TOAST_ERROR = TD.TOAST_ERROR;
+export const TOAST_INFO = TD.TOAST_INFO;
+export const TOAST_SUCCESS = TD.TOAST_SUCCESS;
+export const TOAST_WARNING = TD.TOAST_WARNING;
+
+export const Toaster = T.Toaster;
+export const AnimatedToasters = T.AnimatedToasters;
