320cec9f4ecd5a4b1a781b454eecdca3c55a0f32
Roger Groth <roger.groth@gis.lu.se>
2017-12-20 11:41:22 +0100

    (icos-cp-ol): Added method for calculating min max for a bounding box in a different reference system

--- icos-cp-ol/src/index.js
+++ icos-cp-ol/src/index.js
@@ -4,3 +4,6 @@ import * as P from './Popup';
 export const OL = OLmap.OL;
 export const getViewParams = OLmap.getViewParams;
 export const Popup = P.Popup;
+
+import * as MinMax from './getProjectedMinMax';
+export const getProjectedMinMax = MinMax.getProjectedMinMax;
