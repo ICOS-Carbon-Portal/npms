a82aacd1c3fb0a12755a3cddb80f9dfb02b56010
Roger Groth <roger.groth@gis.lu.se>
2018-01-08 15:15:20 +0100

    Add blue background in the outline extent

--- icos-cp-ol/src/index.js
+++ icos-cp-ol/src/index.js
@@ -2,6 +2,7 @@ import * as OLmap from './OL';
 import * as P from './Popup';
 
 export const OL = OLmap.OL;
+export const supportedSRIDs = OLmap.supportedSRIDs;
 export const getViewParams = OLmap.getViewParams;
 export const Popup = P.Popup;
 
